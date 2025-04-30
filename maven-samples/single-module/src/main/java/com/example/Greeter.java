dfb2d766456e1d022fd7889383e791d14987847e
sekar004 <sekarshanmugam2003@gmail.com>
2025-04-30 17:13:51 +0530

    Update Greeter.java

--- maven-samples/single-module/src/main/java/com/example/Greeter.java
+++ maven-samples/single-module/src/main/java/com/example/Greeter.java
@@ -21,7 +21,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println(" Hello team Good Morning welcome all !!!");
+	  System.out.println(" hello guys & Hello team Good Morning welcome all !!!");
   }
 	
 }
